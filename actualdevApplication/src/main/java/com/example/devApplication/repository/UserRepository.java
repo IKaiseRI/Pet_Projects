6248a637c62c6df12e96470442663080a6fffc8e
Ion Vornovitchi <crme065@cedmd.ced.it>
2023-08-24 16:31:00 +0300

    first push from office

--- actualdevApplication/src/main/java/com/example/devApplication/repository/UserRepository.java
+++ actualdevApplication/src/main/java/com/example/devApplication/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.devApplication.repository;
 
 import com.example.devApplication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//ceva
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
 
